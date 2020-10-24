4e31db9ebe0e6079bece6aa68d00377da3a434a8
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-25 00:05:43 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,29 @@
 var anim_kiki, anim_lala;
 
 function preload(){
+  // アニメーションを読み込む
   anim_kiki = loadAnimation("assets/kiki1.png","assets/kiki4.png");
   anim_lala = loadAnimation("assets/lala1.png","assets/lala4.png");
+  cloud = loadImage('cloud.png');
+  cloud2 = loadImage('cloud.png');
+  cloud3 = loadImage('cloud.png');
+  cloud4 = loadImage('cloud.png');
 }
 	
 function setup(){
   createCanvas(1000, 1000);
-  var sprite = createSprite(100, 100);
-  sprite.addAnimation("kiki", anim_kiki);
-  sprite = createSprite(200, 100); 
-  sprite.addAnimation("lala", anim_lala); 
+  var sprite = createSprite(30, 50); //スプライトの作成
+  sprite.addAnimation("kiki", anim_kiki); //スプライトにアニメーションを加える
+  sprite = createSprite(70, 50); //スプライトの作成
+  sprite.addAnimation("lala", anim_lala); //スプライトにアニメーションを加える
 }
 
 function draw(){
   background(158, 231, 250);
   drawSprites();
+  image(cloud, 300, 600, 300, 200);
+  image(cloud2, 100, 200, 400, 200);
+  image(cloud3, 200, 10, 350, 200);
+  image(cloud4, 400, 300, 500, 300);
 
 }
