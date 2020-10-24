1e4a280981ac0cfc682502c798c7be9bbf658f33
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-25 00:12:14 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,18 +12,18 @@ function preload(){
 	
 function setup(){
   createCanvas(1000, 1000);
-  var sprite = createSprite(300, 100); //スプライトの作成
+  var sprite = createSprite(300, 500); //スプライトの作成
   sprite.addAnimation("kiki", anim_kiki); //スプライトにアニメーションを加える
-  sprite = createSprite(500, 100); //スプライトの作成
+  sprite = createSprite(500, 700); //スプライトの作成
   sprite.addAnimation("lala", anim_lala); //スプライトにアニメーションを加える
 }
 
 function draw(){
   background(158, 231, 250);
   drawSprites();
-  image(cloud, 300, 600, 300, 200);
+  image(cloud, 300, 700, 300, 200);
   image(cloud2, 100, 200, 400, 200);
   image(cloud3, 200, 10, 350, 200);
-  image(cloud4, 400, 300, 500, 300);
+  image(cloud4, 400, 300, 300, 300);
 
 }
